42a8635a861d82465052b9b615bd268bc3e2ba7b
Mário Alexandre Lopes Liberato <mliberato@ua.pt>
2017-04-14 15:50:19 +0100

    Now accepts the full response

--- src/client.py
+++ src/client.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # encoding=utf-8
 import socket
+import sys
+import time
 import json
 
 # Some values
@@ -14,10 +16,38 @@ def main():
     print("Connecting to " + tgt_server[0] + ":" + str(tgt_server[1]))
     sock.connect(tgt_server)
     print(sock.send('{"type":"LIST"}\r\n'.encode()))
-    rv = sock.recv(1024)
+    rv = recvall(sock).decode()
+
     sock.close()
 
     print(rv)
+
+def recvall(sock):
+    sock.setblocking(0)
+    data_array = []
+    data = ""
+
+    start_time = time.time()
+
+    while 1:
+        if data_array and time.time() - start_time > 2:
+            break
+        elif time.time() - start_time > 4:
+            break
+
+        try:
+            data = sock.recv(8192)
+            if data:
+                data_array.append(data)
+                start_time = time.time()
+            else:
+                time.sleep(0.1)
+        except:
+            pass
+
+
+    return b"".join(data_array)
+
 
 # Runs main function
 if __name__ == "__main__":
