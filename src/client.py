7b947b08c740f867c1bb2e6b02586bba5a9c8edf
gnmmarechal <gnmpolicemata91@gmail.com>
2017-04-21 15:00:01 +0100

    Can now read 1 message, all messages, put messages, etc.

--- src/client.py
+++ src/client.py
@@ -38,22 +38,19 @@ def main():
 
 def menu():
     print("PBox Client v" + app_version + "\n")
-    box_list = get_box_list(tgt_server[0], tgt_server[1])
-    if box_list[u"code"] != "OK":
-        print("Server Reply Code: " + BColors.FAIL + str(box_list[u"code"]) + BColors.ENDC + '\n Expected "OK"')
-        sys.exit(-1)
-    if box_list[u"type"] != "RESULT":
-        print("Server Reply Type: " + BColors.FAIL + str(box_list[u"type"]) + BColors.ENDC + '\n Expected "RESULT"')
-        sys.exit(-1)
-
+    box_list = get_box_list(tgt_server)
     box_names = []
     for box in box_list[u"payload"]:
         box_names.append(box[u"name"])
-    box_names = u', '.join(box_names).encode('utf-8').strip()
+    box_names = prettyfy(box_names, ", ")
     print(BColors.BOLD + "Existing Boxes:\n" + BColors.ENDC + box_names + "\n")
     print(BColors.BOLD + "Number of Boxes: " + BColors.ENDC + str(get_box_number(box_list)))
 
-    put_message("84917_lolol", "Boas pessoal aqui estou com mais um video", tgt_server[0], tgt_server[1])
+    print(get_message("84917_lolol", tgt_server))
+    put_message("84917_lolol", "Boas pessoal aqui estou com mais um videowsdf", tgt_server)
+    put_message("84917_lolol", "Boas pessoal aqui estou com mais um video2eq211wsdf", tgt_server)
+    put_message("84917_lolol", "Boas pessoal aqui estou com mais um videoesasdawsdf", tgt_server)
+    print(prettyfy(get_messages("84917_lolol", tgt_server), "\n"))
 
 
 # Other functions
@@ -65,31 +62,102 @@ def validate_string(string, is_message=False):
     return string
 
 
+def prettyfy(element, separator):
+    return (u'' + separator).join(element).encode('utf-8').strip()
+
+
 # Server functions
-def put_message(box_name, message, server_address, server_port):
+
+def get_message(box_name, (server_address, server_port), called_from_gms=False):
+    msg = '{ "type": "GET", "name": "' + validate_string(box_name) + '", "timestamp":' + str(int(time.time())) + "}\r\n"
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        sock.connect((server_address, server_port))
+    except Exception as e:
+        print(BColors.FAIL + "Connection error! Error Message:")
+        print(e)
+        sys.exit(-1)
+    sock.send(msg.encode())
+    reply = net_funcs.recv_all(sock).decode()
+    sock.close()
+    # {"content": "Boas pessoal aqui estou com mais um video", "timestamp": 1492778665, "code": "OK", "type": "RESULT"}
+    # {"content": "Box is empty", "timestamp": 1492778718, "code": "ERROR", "type": "RESULT"}
+    dic_reply = json.loads(reply)
+
+    if not dic_reply[u"code"] == "OK" or not dic_reply[u"type"] == "RESULT":
+        if dic_reply[u"content"] == "Box is empty" and dic_reply[u"code"] == "ERROR":
+            if not called_from_gms:
+                return BColors.FAIL + "There are no messages in the box!" + BColors.ENDC
+            return BColors.FAIL + "There are no messages in the box!" + BColors.ENDC, 1  # Returns 1 as exit code
+        else:
+            print(BColors.FAIL + "Unknown error!" + BColors.ENDC)
+            sys.exit(-1)
+    else:
+        return dic_reply[u"content"]
+
+
+def get_messages2(box_name, box_list, (server_address, server_port)):  # Second possible implementation of the function
+    # get_messages is used instead, as it doesn't need to update box_list to get all messages, as it doesn't rely
+    # on that, and just fetches the messages directly, until it hits the end.
+    return_messages = []
+    msg_count = get_box_msg_number(box_name, box_list)
+    if msg_count == 0:
+        return get_message(box_name, (server_address, server_port), True)[0]
+    c = 0
+    while c < msg_count:
+        return_messages.append(get_message(box_name, (server_address, server_port), True))
+        c += 1
+    return return_messages
+
+
+def get_messages(box_name, (server_address, server_port)):
+    return_messages = []
+    while True:
+        message = get_message(box_name, (server_address, server_port), True)
+        if len(message) == 2:  # If two values are returned, that means the message is empty
+            if return_messages == []:
+                return_messages.append(message[0])
+            break
+        else:
+            return_messages.append(message)
+    return return_messages
+
+
+def put_message(box_name, message, (server_address, server_port)):
     msg = '{ "type": "PUT", "name": "' + validate_string(box_name) + '", "timestamp":' + str(int(time.time())) + ', "content": "' + validate_string(message, True) + '" }\r\n'
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((server_address, server_port))
+    try:
+        sock.connect((server_address, server_port))
+    except Exception as e:
+        print(BColors.FAIL + "Connection error! Error Message:")
+        print(e)
+        sys.exit(-1)
     sock.send(msg.encode())
     reply = net_funcs.recv_all(sock).decode()
     sock.close()
     # {"timestamp": 1492776513, "code": "OK", "type": "RESULT"}
     dic_reply = json.loads(reply)
 
-    if not len(dic_reply) == 3 and not dic_reply[u"code"] == "OK" and not dic_reply[u"type"] == "RESULT":
+    if not len(dic_reply) == 3 or not dic_reply[u"code"] == "OK" or not dic_reply[u"type"] == "RESULT":
         print(BColors.FAIL + "Error sending message." + BColors.ENDC)
         return 1
     else:
         print(BColors.OKGREEN + "Message Sent! Timestamp: " + str(dic_reply[u"timestamp"]) + BColors.ENDC)
         return 0
 
-def create_box(box_name, server_address, server_port, pubk="-1", sig="-1"):  # Creates a box, optional with security
+
+def create_box(box_name, (server_address, server_port), pubk="-1", sig="-1"):  # Creates a box, optional with security
     # SECURITY NOT IMPLEMENTED
     msg = '{ "type": "CREATE", "name": "' + validate_string(box_name) + '", "timestamp":' + str(int(time.time())) + ' }\r\n'
     if pubk != "-1" or sig != "-1":
         msg = '{ "type": "CREATE", "name": "' + validate_string(box_name) + '", timestamp":' + str(int(time.time())) + ', "pubk": "' + pubk + '", "sig": "' + sig + '" }\r\n'  # Not implemented
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((server_address, server_port))
+    try:
+        sock.connect((server_address, server_port))
+    except Exception as e:
+        print(BColors.FAIL + "Connection error! Error Message:")
+        print(e)
+        sys.exit(-1)
     sock.send(msg.encode())
     reply = net_funcs.recv_all(sock).decode()
     sock.close()
@@ -103,28 +171,29 @@ def create_box(box_name, server_address, server_port, pubk="-1", sig="-1"):  # C
                 return "OK"
             else:
                 print(BColors.FAIL + "Error." + BColors.ENDC)
-                exit(-1)
+                sys.exit(-1)
         else:
             print(BColors.FAIL + "Unknown Error." + BColors.ENDC)
-            exit(-1)
+            sys.exit(-1)
     elif len(dic_reply) == 4:  # Checking content of error message
         if dic_reply[u"code"] == "ERROR":
             print(BColors.FAIL + "Error Message:" + BColors.ENDC)
             print(dic_reply[u"content"])
             if not dic_reply[u"content"] == "Box already exists":  # If the error is not the expected one, quit
-                exit(-1)
+                sys.exit(-1)
         else:
             print(BColors.FAIL + "Unknown Error." + BColors.ENDC)
-            exit(-1)
+            sys.exit(-1)
     else:
         print(BColors.FAIL + "Unknown Error." + BColors.ENDC)
-        exit(-1)
+        sys.exit(-1)
 
 
 def get_box_msg_number(box_name, box_list):
     box_index = get_box_index(validate_string(box_name), box_list)
     if box_index == -1:
         print(BColors.FAIL + "Box doesn't exist!" + BColors.ENDC)
+        sys.exit(-1)
 
     return box_list[u"payload"][box_index][u"size"]
 
@@ -134,7 +203,7 @@ def get_box_index(box_name, box_list):
     while index < get_box_number(box_list):
         if box_list[u"payload"][index][u"name"] == box_name:
             return index
-        index = index + 1
+        index += 1
     return -1
 
 
@@ -142,14 +211,26 @@ def get_box_number(box_list):
     return len(box_list[u"payload"])
 
 
-def get_box_list(server_address, server_port):
+def get_box_list((server_address, server_port)):
     msg = '{ "type": "LIST" }\r\n'
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((server_address, server_port))
+    try:
+        sock.connect((server_address, server_port))
+    except Exception as e:
+        print(BColors.FAIL + "Connection error! Error Message:")
+        print(e)
+        sys.exit(-1)
     sock.send(msg.encode())
     reply = net_funcs.recv_all(sock).decode()
     sock.close()
-    return json.loads(reply)
+    dic_reply = json.loads(reply)
+    if dic_reply[u"code"] != "OK":
+        print("Server Reply Code: " + BColors.FAIL + str(dic_reply[u"code"]) + BColors.ENDC + '\n Expected "OK"')
+        sys.exit(-1)
+    if dic_reply[u"type"] != "RESULT":
+        print("Server Reply Type: " + BColors.FAIL + str(dic_reply[u"type"]) + BColors.ENDC + '\n Expected "RESULT"')
+        sys.exit(-1)
+    return dic_reply
 
 
 # CherryPy Part of PBoxClient
@@ -166,7 +247,7 @@ class Actions(object):
     @cherrypy.expose
     def doListBoxes(self):
         print("PBox Client v" + app_version + "\n")
-        box_list = get_box_list(tgt_server[0], tgt_server[1])
+        box_list = get_box_list(tgt_server)
         if box_list[u"code"] != "OK":
             print("Server Reply Code: " + BColors.FAIL + str(box_list[u"code"]) + BColors.ENDC + '\n Expected "OK"')
             sys.exit(-1)
